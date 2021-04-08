ccf6287c58005ce7eba40278a1bbd4ad14df61b4
ddlau <ddlauForGitHub@126.com>
2021-04-09 00:57:27 +0800

    initial

--- tst2nd.py
+++ tst2nd.py
@@ -437,6 +437,8 @@ class TRPO( ActorCriticRLModel ):
 						vpredbefore = seg[ "vpred" ]  # predicted value function before update
 						atarg = (atarg - atarg.mean()) / (atarg.std() + 1e-8)  # standardized advantage function estimate
 
+
+						print( 'advantages: ', np.min(atarg), np.max(atarg), np.mean(atarg))
 						# true_rew is the reward without discount
 						if writer is not None:
 							total_episode_reward_logger( self.episode_reward,
@@ -465,6 +467,7 @@ class TRPO( ActorCriticRLModel ):
 								                                                 options=run_options,
 								                                                 run_metadata=run_metadata )
 
+						print( f'losses before', lossbefore)
 						lossbefore = self.allmean( np.array( lossbefore ) )
 						grad = self.allmean( grad )
 						if np.allclose( grad, 0 ):
@@ -563,33 +566,34 @@ class TRPO( ActorCriticRLModel ):
 
 if __name__ == '__main__':
 
-	from stable_baselines.common.distributions import DiagGaussianProbabilityDistribution
-	np.random.seed(1)
-
-	ppp = np.arange(1,1024*10000+1,1).reshape(10000,1024).astype(np.float32) #np.random.rand(10000,1024).astype(np.float32)
-	prb = DiagGaussianProbabilityDistribution( ppp)
-	x = prb.sample()
-	b = prb.logp(x)
-
-	c =tf.reduce_mean(		prb.entropy())
-	d = tf.reduce_mean(-b)
-	e = tf.math.reduce_std(b)
-	with tf.Session() as session:
-		c = session.run(tf.reduce_mean(		prb.entropy()))
-		print( c)
-		d = session.run( d)
-		print( d)
-		print( c-d)
-		e= session.run(e)
-		print( e)
-
-	exit()
+	# from stable_baselines.common.distributions import DiagGaussianProbabilityDistribution
+	# np.random.seed(1)
+	#
+	# ppp = np.arange(1,1024*10000+1,1).reshape(10000,1024).astype(np.float32) #np.random.rand(10000,1024).astype(np.float32)
+	# prb = DiagGaussianProbabilityDistribution( ppp)
+	# x = prb.sample()
+	# b = prb.logp(x)
+	#
+	# c =tf.reduce_mean(		prb.entropy())
+	# d = tf.reduce_mean(-b)
+	# e = tf.math.reduce_std(b)
+	# with tf.Session() as session:
+	# 	c = session.run(tf.reduce_mean(		prb.entropy()))
+	# 	print( c)
+	# 	d = session.run( d)
+	# 	print( d)
+	# 	print( c-d)
+	# 	e= session.run(e)
+	# 	print( e)
+	#
+	# exit()
 
 
 	env = gym.make('CartPole-v1')
+	env = gym.make( 'Pendulum-v0')
 
 	model = TRPO(MlpPolicy, env, verbose=1)
-	model.learn(total_timesteps=25000)
+	model.learn(total_timesteps=250000)
 	#model.save("trpo_cartpole")
 	#del model # remove to demonstrate saving and loading
 	#model = TRPO.load("trpo_cartpole")
